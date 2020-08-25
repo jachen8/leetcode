0905adaf0abb455d440c04bfcd59be720d0284c3
Jachen <Jachen@JJ.fios-router.home>
2020-08-24 21:20:58 -0400

    optimized goodpairs a bit
    quicker runtime removed nested loop

--- leetcode/src/goodPairs/Solution.java
+++ leetcode/src/goodPairs/Solution.java
@@ -2,6 +2,7 @@ package goodPairs;
 
 //runtime 2ms mem use 39.1mb
 
+/*
 public class Solution {
 	public int numIdenticalPairs(int[] nums) {
 		int count = 0;
@@ -15,4 +16,23 @@ public class Solution {
 		return count;
 	}
 }
+*/
 
+public class Solution{
+	public int numIdenticalPairs(int[] nums) {
+		int count = 0;
+		int i = 0;
+		for(int j = 1; j < nums.length; j++) {
+			if(nums[i] == nums[j] && i<j) {
+				count++;
+			}
+			if(j == nums.length-1) {
+				i++;
+				j = i;
+			}
+		}
+		return count;
+	}
+	
+	
+}
