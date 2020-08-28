25cd045f09bc7deb5d7ce88d4b4d8bbd3ef7f1da
Jachen <Jachen@JJ.fios-router.home>
2020-08-27 21:25:12 -0400

    its wednesday my dudes

---
+++ leetcode/src/jewelsStones/Solution.java
@@ -0,0 +1,13 @@
+package jewelsStones;
+
+public class Solution {
+	public int numJewelsInStones(String J, String S) {
+		int count = 0;
+		for(int i = 0; i < S.length(); i++) {
+			if(J.indexOf(S.charAt(i)) >= 0) {
+				count++;
+			}
+		}
+		return count;
+	}
+}
