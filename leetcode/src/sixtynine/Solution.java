18a72064204bfe528a4ae593b0968e054460e02f
Jachen <Jachen@JJ.fios-router.home>
2020-09-14 19:50:44 -0400

    sped up runtime
    parseint had quicker runtime

--- leetcode/src/sixtynine/Solution.java
+++ leetcode/src/sixtynine/Solution.java
@@ -2,6 +2,6 @@ package sixtynine;
 
 public class Solution {
 	public int maximum69Number(int num) {
-		return Integer.valueOf(String.valueOf(num).replaceFirst("6", "9"));
+		return Integer.parseInt(String.valueOf(num).replaceFirst("6", "9"));
 	}
 }
